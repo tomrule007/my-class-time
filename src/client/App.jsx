933ffc8ae53b61bd53808f0781e42fb09d6b7632
Allister <allisterkmoon@gmail.com>
2019-11-21 11:02:23 -0800

    Add some inputs

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -8,8 +8,54 @@ import Footer from './components/Footer';
 const App = () => (
   <Router>
     <Navbar />
-    <div className="content container">
-      Hello World
+    <div className="content">
+      <section className="container input">
+
+        <div className="row">
+          <h4>Days to have classes</h4>
+          <label htmlFor="wantMonday">
+            Monday
+            <input name="wantMonday" type="checkbox" />
+          </label>
+          <label htmlFor="wantTuesday">
+            Tuesday
+            <input name="wantTuesday" type="checkbox" />
+          </label>
+          <label htmlFor="wantWednesday">
+            Wednesday
+            <input name="wantWednesday" type="checkbox" />
+          </label>
+          <label htmlFor="wantThursday">
+            Thursday
+            <input name="wantThursday" type="checkbox" />
+          </label>
+          <label htmlFor="wantFriday">
+            Friday
+            <input name="wantFriday" type="checkbox" />
+          </label>
+          <label htmlFor="wantSaturday">
+            Saturday
+            <input name="wantSaturday" type="checkbox" />
+          </label>
+          <label htmlFor="wantSunday">
+            Sunday
+            <input name="wantSunday" type="checkbox" />
+          </label>
+        </div>
+
+        <div className="row">
+          <h4>Desired start/end times</h4>
+          <label htmlFor="startTime">
+            Start Time
+            <input name="startTime" type="time" />
+          </label>
+          <label htmlFor="endTime">
+            End Time
+            <input name="endTime" type="time" />
+          </label>
+        </div>
+
+      </section>
     </div>
     <Footer />
   </Router>
